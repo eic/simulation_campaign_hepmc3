d244ad0f42490f15b578651b7a13cc6b8bc1d3ae
Sakib Rahman <rahmans@myumanitoba.ca>
2024-08-14 13:04:08 -0400

    Pass Detector Path as explicit variable
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -37,7 +37,7 @@ test -f .machine.ad && cat .machine.ad
 
 # Load container environment (include ${DETECTOR_VERSION})
 export DETECTOR_CONFIG_REQUESTED=${DETECTOR_CONFIG:-}
-export DETECTOR_PATH=/opt/detector/epic-${DETECTOR_VERSION:-main}/share/epic
+source ${DETECTOR_PATH:-opt/detector/epic-main}/bin/thisepic.sh
 export DETECTOR_CONFIG=${DETECTOR_CONFIG_REQUESTED:-${DETECTOR_CONFIG:-$DETECTOR}}
 
 # Argument parsing

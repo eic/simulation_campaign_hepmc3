d5bb4033c0d35c885048dc711e2d50322e109a60
Sakib Rahman <rahmans@myumanitoba.ca>
2024-12-14 10:51:20 -0500

    Introduce argument parser and a few cosmetic changes

--- scripts/register_to_rucio.py
+++ scripts/register_to_rucio.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
+import argparse
+import os
 from rucio.client import Client
 from rucio.common import exception
 from rucio.common.utils import adler32
 
-parent_dir="$1"
-scope="$RSESCOPE"
+parser = argparse.ArgumentParser(prog='Register to RUCIO', description='Registers files to RUCIO')
+
+parser.add_argument("-f", dest="file_path", action="store", required=True, help="Enter the file path")
+parser.add_argument("--du", dest="file_size", action="store", required=True, help="Enter the file size")
+                    
+args=parser.parse_args()
+
+rse_url="https://dtn-rucio.jlab.org:1094/"     # Change this to an array later to add BNL rucio
+rse_name="EIC-XRD"                             
+scope="epic"
+file_path = args.file_path
+file_size = args.file_size
+parent_dir=os.path.dirname(file_path)
 
 try:
     client.add_dataset(scope=scope, name=parent_dir)
@@ -14,22 +27,19 @@ except exception.DataIdentifierAlreadyExists:
 except Exception as e:
     print(f"Dataset {parent_dir} failed to add. error: {e}")
 
-file_path = "$2"
-file_size = "$3"
-pfn =  RSE_PATH + file_path
-replicas = [{
+pfn =  rse_url + file_path
+replicas = [{                                 # Change this to an array later to add BNL rucio
     'scope': scope,
     'name': file_path,
     'bytes': file_size,
     'adler32': adler32(file_path),
-    #'md5': md5(file_path),
     'pfn': pfn
-}]
+}]   
 
 try:
-    client.add_replicas(rse=RSE, files=replicas)
+    client.add_replicas(rse=rse_name, files=replicas)
 except exception.FileReplicaAlreadyExists:
-    print(f"file replicas already exists")  # noqa: F541
+    print(f"file replicas already exists") 
 except Exception as e:
     print(f"Error adding replicas: {e}")
 
